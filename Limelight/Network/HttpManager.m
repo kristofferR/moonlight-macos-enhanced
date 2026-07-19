2961f4a93bdc142b0080d0a41032c0779326be78
Kristoffer <481270+kristofferR@users.noreply.github.com>
2026-07-19 02:48:55 +0200

    Revert "Mirror the host desktop from the Desktop tile"
    This reverts commit ddf3f5936e30d58ec47cd5b19f2681b5bda2d8cf.

--- Limelight/Network/HttpManager.m
+++ Limelight/Network/HttpManager.m
@@ -417,17 +417,6 @@ static const NSString* HTTPS_PORT = @"47984";
     }
 }
 
-- (void)appendPolarisDesktopDisplayParam:(NSMutableString *)extraParams
-                                  config:(StreamConfiguration *)config {
-    NSString *appName = [config.appName stringByTrimmingCharactersInSet:
-                         [NSCharacterSet whitespaceAndNewlineCharacterSet]];
-    if ([appName caseInsensitiveCompare:@"Desktop"] == NSOrderedSame) {
-        // Keep game entries on Polaris' private compositor, but make the
-        // standard Desktop tile mirror the already-running host workspace.
-        [extraParams appendString:@"&mirrorDesktop=1"];
-    }
-}
-
 - (NSURLRequest*) newLaunchRequest:(StreamConfiguration*)config {
     BOOL sops = config.optimizeGameSettings;
 
@@ -488,7 +477,6 @@ static const NSString* HTTPS_PORT = @"47984";
     }
 
     [self appendSunshineFoundationParams:extraParams config:config];
-    [self appendPolarisDesktopDisplayParam:extraParams config:config];
 
     // Ensure even dimensions (some encoders/decoders require this)
     modeWidth &= ~1;
@@ -573,7 +561,6 @@ static const NSString* HTTPS_PORT = @"47984";
     }
 
     [self appendSunshineFoundationParams:extraParams config:config];
-    [self appendPolarisDesktopDisplayParam:extraParams config:config];
 
     modeWidth &= ~1;
     modeHeight &= ~1;
